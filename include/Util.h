e331e2723f70e1e8ada58f4a60e0589ca47b9038
mynameisjohn <mynameisjohnj@gmail.com>
2016-11-29 14:54:11 -1000

    Adding util header

---
+++ include/Util.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <functional>
+template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
+struct fun_ptr_helper
+{
+public:
+	typedef std::function<_Res( _ArgTypes... )> function_type;
+
+	static void bind( function_type&& f )
+	{
+		instance().fn_.swap( f );
+	}
+
+	static void bind( const function_type& f )
+	{
+		instance().fn_ = f;
+	}
+
+	static _Res invoke( _ArgTypes... args )
+	{
+		return instance().fn_( args... );
+	}
+
+	typedef decltype( &fun_ptr_helper::invoke ) pointer_type;
+	static pointer_type ptr()
+	{
+		return &invoke;
+	}
+
+private:
+	static fun_ptr_helper& instance()
+	{
+		static fun_ptr_helper inst_;
+		return inst_;
+	}
+
+	fun_ptr_helper() {}
+
+	function_type fn_;
+};
+
+template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
+typename fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::pointer_type
+get_fn_ptr( const std::function<_Res( _ArgTypes... )>& f )
+{
+	fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::bind( f );
+	return fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::ptr();
+}
+
+template<typename T>
+std::function<typename std::enable_if<std::is_function<T>::value, T>::type>
+make_function( T *t )
+{
+	return { t };
+}
+
+// Open a raw image file, implemented in filereader.cpp
+#include <opencv2/core.hpp>
+cv::Mat Raw2Mat( void * pData, size_t uNumBytes );
+cv::Mat Raw2Mat( std::string strFileName );
